8f2ed011a85912c4575a9fa622a8325edeb100c4
harolp0810 <harold.p2329@gmail.com>
2022-08-16 21:21:45 -0500

    Discount

---
+++ src/main/java/com/fourseasons/edu/uco/fourseasonsapi/application/dto/DiscountTypeDTO.java
@@ -0,0 +1,15 @@
+package com.fourseasons.edu.uco.fourseasonsapi.application.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class DiscountTypeDTO {
+    private Long id;
+
+}
---
+++ src/main/java/com/fourseasons/edu/uco/fourseasonsapi/infrastructure/controller/DiscoutTypeController.java
@@ -0,0 +1,45 @@
+package com.fourseasons.edu.uco.fourseasonsapi.infrastructure.controller;
+
+
+import com.fourseasons.edu.uco.fourseasonsapi.application.dto.DiscountTypeDTO;
+import com.fourseasons.edu.uco.fourseasonsapi.application.dto.SeasonDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("/discount")
+
+public class DiscoutTypeController {
+    @GetMapping("/discount/{id}")
+    public ResponseEntity<DiscountTypeDTO> findDiscountById(@RequestParam("id") Long id) {
+        return ok(new DiscountTypeDTO());
+    }
+
+    @PostMapping("/discount")
+    public ResponseEntity<DiscountTypeDTO> createDiscount() {
+        return ok(new DiscountTypeDTO());
+    }
+
+    @PutMapping("/discount")
+    public ResponseEntity<DiscountTypeDTO> updateDiscount() {
+        return ok(new DiscountTypeDTO());
+    }
+
+    @DeleteMapping("/discounts")
+    public ResponseEntity<DiscountTypeDTO> deleteAllDiscount() {
+        return ok(new DiscountTypeDTO());
+    }
+
+    @GetMapping("/discounts/prices")
+    public ResponseEntity<List<Double>> getDiscount() {
+
+        return ok(new ArrayList<>());
+    }
+
+
+}
