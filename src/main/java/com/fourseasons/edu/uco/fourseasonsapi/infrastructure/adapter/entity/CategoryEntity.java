fd737cf8255e39ce1233ee3462f21cc84989fce1
isabella <you@example.com>
2022-09-05 20:57:59 -0500

    Entity category

--- src/main/java/com/fourseasons/edu/uco/fourseasonsapi/infrastructure/adapter/entity/CategoryEntity.java
+++ src/main/java/com/fourseasons/edu/uco/fourseasonsapi/infrastructure/adapter/entity/CategoryEntity.java
@@ -1,11 +1,23 @@
 package com.fourseasons.edu.uco.fourseasonsapi.infrastructure.adapter.entity;
 
-import javax.persistence.*;
+import lombok.Data;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
 @Entity
 @Table (name="Category")
 public class CategoryEntity {
     @Id
     @GeneratedValue (strategy = GenerationType.SEQUENCE)
     private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private int idProduct;
+
 }
