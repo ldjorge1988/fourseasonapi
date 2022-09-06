90e3c9f5a0fe0f715661ef7d0eabadce912edebf
Jorge Lima <ldjorge1988@gmail.com>
2022-09-05 21:06:05 -0500

    refactorizando @Data

--- src/main/java/com/fourseasons/edu/uco/fourseasonsapi/infrastructure/adapter/entity/CategoryEntity.java
+++ src/main/java/com/fourseasons/edu/uco/fourseasonsapi/infrastructure/adapter/entity/CategoryEntity.java
@@ -1,12 +1,17 @@
 package com.fourseasons.edu.uco.fourseasonsapi.infrastructure.adapter.entity;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table (name="Category")
 public class CategoryEntity {
@@ -20,4 +25,16 @@ public class CategoryEntity {
     @NotNull
     private int idProduct;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        CategoryEntity that = (CategoryEntity) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
