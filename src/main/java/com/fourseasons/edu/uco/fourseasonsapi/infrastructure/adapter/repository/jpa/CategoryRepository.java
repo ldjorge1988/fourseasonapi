0bbc8ccf72f8eb2a53f906b79477df02b9b27a76
isabella <you@example.com>
2022-09-05 21:19:26 -0500

    Repository category

---
+++ src/main/java/com/fourseasons/edu/uco/fourseasonsapi/infrastructure/adapter/repository/jpa/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.fourseasons.edu.uco.fourseasonsapi.infrastructure.adapter.repository.jpa;
+
+import com.fourseasons.edu.uco.fourseasonsapi.infrastructure.adapter.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity,long> {
+
+}
