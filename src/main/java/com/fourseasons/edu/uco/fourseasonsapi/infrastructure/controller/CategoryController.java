7026d1465ab4d2e544fb7a094c2600d67b4e7a5c
isabella <you@example.com>
2022-09-13 23:49:22 -0500

    Changes in category

--- src/main/java/com/fourseasons/edu/uco/fourseasonsapi/infrastructure/controller/CategoryController.java
+++ src/main/java/com/fourseasons/edu/uco/fourseasonsapi/infrastructure/controller/CategoryController.java
@@ -2,6 +2,10 @@ package com.fourseasons.edu.uco.fourseasonsapi.infrastructure.controller;
 
 import com.fourseasons.edu.uco.fourseasonsapi.application.dto.CategoryDTO;
 import com.fourseasons.edu.uco.fourseasonsapi.application.dto.ProductDTO;
+import com.fourseasons.edu.uco.fourseasonsapi.application.dto.response.GenericResponseDTO;
+import com.fourseasons.edu.uco.fourseasonsapi.application.service.CategoryListService;
+import com.fourseasons.edu.uco.fourseasonsapi.application.service.ProductListService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +15,11 @@ import java.util.List;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/inventory")
 public class CategoryController {
 
+    private final CategoryListService categoryListService;
     @GetMapping("/category/{id}")
     public ResponseEntity<CategoryDTO> findCategoryById(@RequestParam("id") Long id) {
         return ok(new CategoryDTO());
@@ -40,8 +46,8 @@ public class CategoryController {
     }
 
     @GetMapping("/categories")
-    public ResponseEntity<List<CategoryDTO>> getAllProducts() {
-        return ok(new ArrayList<>());
+    public ResponseEntity<GenericResponseDTO> getAllCategories() {
+        return ok(categoryListService.execute());
     }
 
 }
