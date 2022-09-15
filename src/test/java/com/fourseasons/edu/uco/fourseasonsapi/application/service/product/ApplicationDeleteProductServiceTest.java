d02834a1af69c27e941be950da10c775276e1d18
Jorge Lima <ldjorge1988@gmail.com>
2022-09-14 21:16:40 -0500

    implementacion creacion y eliminacion de product

---
+++ src/test/java/com/fourseasons/edu/uco/fourseasonsapi/application/service/product/ApplicationDeleteProductServiceTest.java
@@ -0,0 +1,39 @@
+package com.fourseasons.edu.uco.fourseasonsapi.application.service.product;
+
+import com.fourseasons.edu.uco.fourseasonsapi.application.dto.ProductDTO;
+import com.fourseasons.edu.uco.fourseasonsapi.application.mapper.ApplicationProductMapper;
+import com.fourseasons.edu.uco.fourseasonsapi.domain.model.Product;
+import com.fourseasons.edu.uco.fourseasonsapi.domain.service.ProductDeleteService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplicationDeleteProductServiceTest {
+
+    @InjectMocks
+    ApplicationDeleteProductService deleteProductService;
+
+    @Mock
+    ApplicationProductMapper productMapper;
+
+    @Mock
+    ProductDeleteService productDeleteService;
+
+    @Mock
+    ProductDTO productDTO;
+
+    @Mock
+    Product product;
+
+    @Test
+    void execute(){
+        deleteProductService.execute(productDTO);
+        productDeleteService.execute(product);
+        verify(productMapper).toDomain(productDTO);
+    }
+}
---
+++ src/test/java/com/fourseasons/edu/uco/fourseasonsapi/application/service/product/ApplicationSaveProductServiceTest.java
@@ -0,0 +1,38 @@
+package com.fourseasons.edu.uco.fourseasonsapi.application.service.product;
+
+import com.fourseasons.edu.uco.fourseasonsapi.application.dto.ProductDTO;
+import com.fourseasons.edu.uco.fourseasonsapi.application.mapper.ApplicationProductMapper;
+import com.fourseasons.edu.uco.fourseasonsapi.domain.model.Product;
+import com.fourseasons.edu.uco.fourseasonsapi.domain.service.ProductSaveService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplicationSaveProductServiceTest {
+
+    @InjectMocks
+    ApplicationSaveProductService saveProductService;
+
+    @Mock
+    ProductSaveService productSaveService;
+    @Mock
+    ApplicationProductMapper productMapper;
+
+    @Mock
+    ProductDTO productDTO;
+
+    @Mock
+    Product product;
+
+    @Test
+    void execute(){
+        saveProductService.execute(productDTO);
+        productSaveService.execute(product);
+        verify(productMapper).toDomain(productDTO);
+    }
+}
